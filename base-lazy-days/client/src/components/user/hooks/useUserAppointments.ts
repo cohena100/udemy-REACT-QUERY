03eda761f15726a46f45502ec578a12c166b68ab
Avi Cohen <cohena100@gmail.com>
2024-07-04 10:39:46 +0300

    Update useUserAppointments.ts

--- base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,9 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+
 import type { Appointment } from "@shared/types";
 
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 
 import { useLoginData } from "@/auth/AuthContext";
-import { useQuery } from "@tanstack/react-query";
 import { generateAppointmentsKey } from "@/react-query/key-factories";
 
 // for when we need a query function for useQuery
